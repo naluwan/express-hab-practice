e1f4ce5430d0385238ff42a6800fa79d16eb9f8e
naluwan <street.baller06@gmail.com>
2023-01-11 10:55:21 +0800

    feat: add all router and update index.hbs & style.css

--- app.js
+++ app.js
@@ -11,11 +11,27 @@ const port = process.env.PORT || 3030;
 app.engine("hbs", engine({ extname: ".hbs", helpers: handlebarsHelpers }));
 app.set("view engine", "hbs");
 
-// 設定express取用靜態檔案
+// 設定express取用靜態檔案(js, css)
 app.use(express.static("public"));
 
+// 首頁 router
 app.get("/", (req, res) => {
   res.render("index", { currentPage: "首頁" });
+});
+
+// about頁 router
+app.get("/about", (req, res) => {
+  res.render("index", { currentPage: "About" });
+});
+
+// profileInfo頁 router
+app.get("/profileInfo", (req, res) => {
+  res.render("index", { currentPage: "ProfileInfo" });
+});
+
+// contact頁 router
+app.get("/contact", (req, res) => {
+  res.render("index", { currentPage: "Contact" });
 });
 
 // 監聽啟動的伺服器
